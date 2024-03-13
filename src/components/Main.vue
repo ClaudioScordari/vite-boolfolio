ee8869758e190ef13fd85cee64046e1728c84db9
ClaudioScordari <scordari22@gmail.com>
2024-03-13 20:46:28 +0100

    Collegate repo, aggiunto axios e stampato in console array dei projects

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            datas: [],
+        }
+    },
+    created() {
+        axios  
+            .get('http://127.0.0.1:8000/api/projects')
+            .then(response => {
+                console.log(response.data.response.data);
+            });
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Componente principale
+    </h1>
+
+    <div class="main-content">
+
+    </div>
+</template>
+
+<style scoped>
+</style>
