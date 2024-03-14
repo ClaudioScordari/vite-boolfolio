a4138d9e006f3ea21d2977cc0b36380a277c870d
ClaudioScordari <scordari22@gmail.com>
2024-03-14 20:08:04 +0100

    Aggiunta funzionalità avanti e indietro pagine

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,19 +5,46 @@ export default {
     data() {
         return {
             datas: [],
+            currentPage: 1,
+            lastPage: 1,
         }
     },
     created() {
-        axios  
-            .get('http://127.0.0.1:8000/api/projects')
-            .then(response => {
-                // console.log(response.data.response);
-
-                // Array con tutti i dati 
-                this.datas = response.data.response;
-                console.log(this.datas);
-            });
+        this.getDatas(this.currentPage);
     },
+    methods: {
+        getDatas(page){
+            axios  
+                .get('http://127.0.0.1:8000/api/projects',{
+                    params: {
+                        page: page,
+                    }
+                })
+                .then(response => {
+                    // console.log(response.data.response);
+
+                    // Array con tutti i dati 
+                    this.datas = response.data.response.data;
+
+                    // Imposto il valore della pagina corrente e dell'ultima
+                    this.currentPage = response.data.response.current_page
+                    this.lastPage = response.data.response.last_page
+                    // console.log(this.currentPage, this.lastPage)
+                });
+        },
+        nextPage(){
+            if (this.currentPage < this.lastPage) {
+
+                this.getDatas(this.currentPage + 1);
+            }
+        },
+        prevPage(){
+            if (this.currentPage > 1) {
+
+                this.getDatas(this.currentPage - 1);
+            }
+        },
+    }
 }
 </script>
 
@@ -27,6 +54,10 @@ export default {
 
         <div class="card-body">
             <h5 class="card-title">
+                <span>
+                    {{ project.id }})
+                </span>
+
                 {{ project.name }}
             </h5>
 
@@ -36,6 +67,11 @@ export default {
 
             <a href="#" class="btn btn-primary">Vedi</a>
         </div>
+    </div>
+
+    <div class="container box-buttons mt-4 d-flex justify-content-around">
+        <button @click="prevPage()" class="btn btn-primary" type="submit">Indetro</button>
+        <button @click="nextPage()" class="btn btn-primary" type="submit">Avanti</button>
     </div>
 </template>
 
